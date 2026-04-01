e2941875d84436475ff4ed4c0d38e185513d9f4a
albertzaw <albertzaw2208@gmail.com>
2026-04-02 00:24:39 +0800

    Move HealthCheckController.java to main/controller folder

--- src/main/java/com/albert/springbootbackend/controllers/HealthCheckController.java
+++ src/main/java/com/albert/springbootbackend/controllers/HealthCheckController.java
@@ -2,10 +2,12 @@ package com.albert.springbootbackend.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RequestMapping("/api/v1")
 @RestController
 public class HealthCheckController {
 
